c008f1f22502b18d9f66826d20ceb475c2d1de7d
Jairotsb <jairo_tunisse@outlook.com>
2023-10-31 23:22:39 -0300

    feat: post template added but not finished

---
+++ src/pages/Post/index.tsx
@@ -0,0 +1,20 @@
+export function Post() {
+  return (
+    <PostContainer>
+      <PostContainerCard>
+        <PostHeader>
+          <PostBack></PostBack>
+          <PostLink></PostLink>
+        </PostHeader>
+
+        <PostTitle></PostTitle>
+
+        <PostInfo>
+          <PostNickName></PostNickName>
+          <PostTime></PostTime>
+          <PostComment></PostComment>
+        </PostInfo>
+      </PostContainerCard>
+    </PostContainer>
+  )
+}
