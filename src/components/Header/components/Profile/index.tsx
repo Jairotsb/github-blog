52e3aca9d6badeb35d8a5743fb1c9dc1418c95cf
Jairotsb <jairo_tunisse@outlook.com>
2023-11-02 19:10:28 -0300

    feat: github api connected :muscle:

--- src/components/Header/components/Profile/index.tsx
+++ src/components/Header/components/Profile/index.tsx
@@ -20,6 +20,8 @@ import {
   ProfileName,
 } from './styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+import { api } from '../../../../api'
 
 library.add(
   faS,
@@ -30,36 +32,54 @@ library.add(
   faArrowUpRightFromSquare,
 )
 
+interface UserProps {
+  name: string
+  login: string
+  company: string
+  followers: number
+  following: number
+  bio: string
+  html_url: string
+}
+
 export function Profile() {
+  const [user, setUser] = useState<UserProps>(Object)
+
+  async function fetchUser() {
+    const response = await api.get('')
+    console.log(response.data)
+    setUser(response.data)
+  }
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
   return (
     <ProfileContainer>
       <Avatar />
 
       <ProfileContent>
         <ProfileHeader>
-          <ProfileName>Jairo Tunisse</ProfileName>
-          <GithubLink href="https://www.github.com/Jairotsb" target="_blank">
+          <ProfileName>{user.name}</ProfileName>
+          <GithubLink href={user.html_url} target="_blank">
             GITHUB
             <FontAwesomeIcon icon={['fas', 'arrow-up-right-from-square']} />
           </GithubLink>
         </ProfileHeader>
 
-        <ProfileBio>
-          Full Stack Developer 💙 | Open source | TypeScript | ReactJs | NodeJs
-          | React Native
-        </ProfileBio>
+        <ProfileBio>{user.bio}</ProfileBio>
 
         <ProfileInfo>
           <ProfileInfoContent>
             <FontAwesomeIcon icon={['fab', 'github']} />
-            jairotsb
+            {user.login}
           </ProfileInfoContent>
           <ProfileInfoContent>
-            <FontAwesomeIcon icon={['fas', 'building']} /> eteabs
+            <FontAwesomeIcon icon={['fas', 'building']} /> {user.company}
           </ProfileInfoContent>
           <ProfileInfoContent>
             <FontAwesomeIcon icon={['fas', 'user-group']} />
-            21
+            {user.followers}
           </ProfileInfoContent>
         </ProfileInfo>
       </ProfileContent>
