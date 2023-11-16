da20ccc51fb1a996a0f393467555cd4a38b5da19
Jairotsb <jairo_tunisse@outlook.com>
2023-11-15 22:30:29 -0300

    feat: searchForm has automatically :boom:

--- src/components/SearchForm/components/SearchFormHeader/index.tsx
+++ src/components/SearchForm/components/SearchFormHeader/index.tsx
@@ -1,14 +1,19 @@
+import { useContextSelector } from 'use-context-selector';
 import {
   SearchFormHeaderContainer,
   SearchFormSubtitle,
   SearchFormTitle,
 } from './styles'
+import { GithubContext } from '../../../../contexts/GithubContext';
 
 export function SearchFormHeader() {
+  const posts = useContextSelector(GithubContext, (context) => {
+    return context.posts;
+  });
   return (
     <SearchFormHeaderContainer>
       <SearchFormTitle>Publicações</SearchFormTitle>
-      <SearchFormSubtitle> 6 publicações</SearchFormSubtitle>
+      <SearchFormSubtitle>{posts.length} {posts.length > 1 ? 'publicações' : 'publicação'}</SearchFormSubtitle>
     </SearchFormHeaderContainer>
   )
 }
